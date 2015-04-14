6b1aae38df085cdbbddf6f7c88dc0a2632938ab3
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2015-04-14 20:43:22 +0300

    new parameters

--- Model/Service.php
+++ Model/Service.php
@@ -13,12 +13,14 @@ class Service
      * @param int    $count
      * @param int    $page
      * @param int    $size
+     * @param int    $range
      * @param array  $pageParameters
+     * @param string $pageParameter
      *
      * @return Pagination
      */
-    public function paginate($route, $count, $page = 1, $size = 10, array $pageParameters = array())
+    public function paginate($route, $count, $page = 1, $size = 10, $range = 10, array $pageParameters = array(), $pageParameter = 'page')
     {
-        return new Pagination($route, $count, $page, $size, $pageParameters);
+        return new Pagination($route, $count, $page, $size, $range, $pageParameters, $pageParameter);
     }
 }
